6c912b175a0d49752edf8e1e323677d53e17a481
Paulpanther <paul.methfessel@web.de>
2017-12-30 16:52:59 +0100

    Added MidiInstrument#getInstruments

--- src/de/lep/rmg/out/model/MidiInstrument.java
+++ src/de/lep/rmg/out/model/MidiInstrument.java
@@ -1,5 +1,8 @@
 package de.lep.rmg.out.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author paul
  * @since 29.12.17.
@@ -15,6 +18,8 @@ public class MidiInstrument {
 							TENOR_SAX = 67,
 							TRUMPET = 57,
 							XYLOPHONE = 14;
+
+	private static Map<Integer, String> instruments = new HashMap<>();
 
 	private String name, shortName;
 	private int midiProgram;
@@ -40,29 +45,45 @@ public class MidiInstrument {
 	}
 
 	public static MidiInstrument getInstrument(int type, float volume) {
+		Map<Integer, String> instr = getInstruments();
 		switch (type) {
 			case ACOUSTIC_BASS:
-				return new MidiInstrument("Acoustic Bass", "Bass", type, volume);
+				return new MidiInstrument(instr.get(ACOUSTIC_BASS), "Bass", type, volume);
 			case ACOUSTIC_GUITAR:
-				return new MidiInstrument("Acoustic Guitar", "Git", type, volume);
+				return new MidiInstrument(instr.get(ACOUSTIC_GUITAR), "Git", type, volume);
 			case CELLO:
-				return new MidiInstrument("Cello", "Cel", type, volume);
+				return new MidiInstrument(instr.get(CELLO), "Cel", type, volume);
 			case FLUTE:
-				return new MidiInstrument("Flute", "Flu", type, volume);
+				return new MidiInstrument(instr.get(FLUTE), "Flu", type, volume);
 			case ORGAN:
-				return new MidiInstrument("Organ", "Org", type, volume);
+				return new MidiInstrument(instr.get(ORGAN), "Org", type, volume);
 			case PIANO:
-				return new MidiInstrument("Piano", "Pia", type, volume);
+				return new MidiInstrument(instr.get(PIANO), "Pia", type, volume);
 			case TRUMPET:
-				return new MidiInstrument("Trumpet", "Tru", type, volume);
+				return new MidiInstrument(instr.get(TRUMPET), "Tru", type, volume);
 			case TENOR_SAX:
-				return new MidiInstrument("Tenor Saxophone",
+				return new MidiInstrument(instr.get(TENOR_SAX),
 						"TSax", type, volume);
 			case XYLOPHONE:
-				return new MidiInstrument("Xylophone", "Xyl", type, volume);
+				return new MidiInstrument(instr.get(XYLOPHONE), "Xyl", type, volume);
 			default:
 				return new MidiInstrument("Unnamed", "Unn", type, volume);
 		}
+	}
+
+	public static Map<Integer, String> getInstruments() {
+		if (instruments.size() == 0) {
+			instruments.put(26, "Acoustic Guitar");
+			instruments.put(26, "Acoustic Bass");
+			instruments.put(26, "Cello");
+			instruments.put(26, "Flute");
+			instruments.put(26, "Organ");
+			instruments.put(26, "Piano");
+			instruments.put(26, "Tenor Saxophone");
+			instruments.put(26, "Trumpet");
+			instruments.put(26, "Xylophone");
+		}
+		return instruments;
 	}
 
 	public String getName() {
